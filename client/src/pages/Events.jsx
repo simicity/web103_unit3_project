41aa0a04940b61e38bc208e67b6ce7a464810934
simicity <simi40nm@gmail.com>
2023-09-30 22:35:35 -0700

    Add filtering function

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -2,9 +2,24 @@ import React, { useState, useEffect } from 'react'
 import Event from '../components/Event'
 import '../css/LocationEvents.css'
 import EventsAPI from '../services/EventsAPI.js'
+import LocationsAPI from '../services/LocationsAPI.js'
 
 const Events = () => {
+    const [locations, setLocations] = useState([])
+    const [selectedLocation, setSelectedLocation] = useState(0)
     const [events, setEvents] = useState([])
+
+    useEffect(() => {
+      (async () => {
+          try {
+              const locationsData = await LocationsAPI.getAllLocations()
+              setLocations(locationsData)
+          }
+          catch (error) {
+              throw error
+          }
+      }) ()
+    }, [])
 
     useEffect(() => {
         (async () => {
@@ -18,11 +33,30 @@ const Events = () => {
         }) ()
     }, [])
 
+    const handleSelectChange = async (event) => {
+      setSelectedLocation(event.target.value)
+      if(event.target.value == 0) {
+        const eventData = await EventsAPI.getAllEvents()
+        setEvents(eventData)
+      } else {
+        const eventData = await EventsAPI.getEventsByLocation(event.target.value)
+        setEvents(eventData)          
+      }
+    }
+
     return (
         <div className='location-events'>
             <header>
                 <div className='search-bar'>
-                    <h2>Search Bar Under Construction...</h2>
+                  <label htmlFor="filter-location">Location</label>
+                  <select id="filter-location" defaultValue={0} onChange={handleSelectChange}>
+                    <option key={"location-all"} value={0}>All</option>
+                    {
+                      locations && locations.length > 0 && locations.map((location) => (
+                        <option key={location.name} value={location.id}>{location.name}</option>
+                      ))
+                    }
+                  </select>
                 </div>
             </header>
 
